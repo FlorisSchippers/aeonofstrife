595dea9cb3974d685b7054a1b1322a81ec47ebcd
Floris Schippers <FlorisSchippers@gmail.com>
2018-05-28 13:57:30 +0200

    Updated appVars and readme

--- readme.md
+++ readme.md
@@ -1,7 +1,7 @@
 # Aeon of Strife #
 ## What is this repository? ##
 
-Competitive eSports platform using React + Firecloud for AeonOfStrife.nl
+Competitive eSports platform using React + Google's Cloud Firestore for AeonOfStrife.nl
 
 ### Who do I talk to? ###
 For questions about this repository you can address Floris Schippers
--- src/config/appVars.js
+++ src/config/appVars.js
@@ -1,10 +1,6 @@
 const appVars = {
     url: `${window.location.protocol}//${window.location.host}`,
-    title: 'Stageverslag',
-    api: {
-        local: 'https://stageverslag.local/wp-json/wp/v2/',
-        live: 'https://wordpress.florisschippers.nl/index.php/wp-json/wp/v2/',
-    },
+    title: `Aeon of Strife`,
     theme: {
         // Green
         // lighter: '#DDFAA4',
@@ -12,12 +8,14 @@ const appVars = {
         // primary: '#7B9F35',
         // dark: '#425B10',
         // darker: '#101800',
+
         // Red
         lighter: '#FBE0E3',
         light: '#F75C6B',
         primary: '#E0091E',
         dark: '#99000F',
         darker: '#4D0007',
+
         // Blue
         // lighter: '#DCE4E5',
         // light: '#7798AB',
